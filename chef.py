20b6b43cd8a2d031759819de658c89225f9d7289
gauravkappor07 <gauravkapoor8834@gmail.com>
2017-07-10 14:00:46 +0530

    code chef chef
    codechef july challange (2nd) of sequence

---
+++ chef.py
@@ -0,0 +1,24 @@
+def p(s):
+	longest = 1
+	count = 1
+	length = len(s)
+	for i in range(length):
+		if s[i] == '<' :
+			count = count+1
+			if count > longest :
+				longest = count
+		elif s[i] == '>' :
+			count = 1
+		elif s[i] == '=':
+			pass
+	print(longest)
+	
+t = int(input())
+test= []
+for i in range(t):
+	demo_test = input()
+	test.append(demo_test)
+	demo_test = ""
+for i in range(t):
+	p(test[i])
+
